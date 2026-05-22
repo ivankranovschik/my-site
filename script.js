be278d0ae8f5c76f7bd01f93c2bc522e12b2bd9c
ivankranovschik <ivankranovschik@gmail.com>
2026-05-22 10:39:27 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+function handleLogin() {
+    document.getElementById('loginSection').style.display = 'none';
+    document.getElementById('mainMenu').style.display = 'block';
+}
+
+function showLogin() {
+    document.getElementById('loginSection').style.display = 'block';
+    document.getElementById('mainMenu').style.display = 'none';
+}
+
+function showObjects() {
+    document.getElementById('objects').style.display = 'block';
+    document.getElementById('settings').style.display = 'none';
+}
+
+function showCities() {
+    document.getElementById('cities').style.display = 'block';
+}
