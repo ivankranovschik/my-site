d5df6b0d3da470e563b5ca5f1a3020f0588fc11e
ivankranovschik <ivankranovschik@gmail.com>
2026-06-01 21:56:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,93 @@
 let power = true;
-let floor = 1;
+let currentFloor = 1;
+let playerInside = false;
 
-function togglePower() {
+function togglePower(){
+
     power = !power;
 
     document.getElementById("power").innerText =
         "Сеть: " + (power ? "ВКЛ" : "ВЫКЛ");
 }
 
-function goToFloor(targetFloor) {
+function callLift(){
 
-    if (!power) {
-        alert("Нет питания!");
+    if(!power){
+        alert("Лифт отключён");
         return;
     }
 
-    floor = targetFloor;
+    document.getElementById("status").innerText =
+        "Лифт едет...";
 
-    document.getElementById("floorText").innerText =
-        "Этаж: " + floor;
+    closeDoors();
 
-    let lift = document.getElementById("lift");
+    setTimeout(() => {
 
-    lift.style.transform =
-        `translateY(${-floor * 20}px)`;
+        currentFloor = 1;
+
+        document.getElementById("status").innerText =
+            "Лифт прибыл";
+
+        openDoors();
+
+    },3000);
+}
+
+function goToFloor(floor){
+
+    if(!playerInside){
+        alert("Сначала войдите в лифт");
+        return;
+    }
+
+    closeDoors();
+
+    document.getElementById("status").innerText =
+        "Едем на этаж " + floor;
+
+    setTimeout(() => {
+
+        currentFloor = floor;
+
+        document.getElementById("status").innerText =
+            "Прибыли на этаж " + floor;
+
+        openDoors();
+
+    },3000);
+}
+
+function enterLift(){
+
+    if(currentFloor !== 1){
+        alert("Лифт не на этаже");
+        return;
+    }
+
+    playerInside = true;
+
+    document.getElementById("insidePanel").style.display =
+        "block";
+
+    document.getElementById("status").innerText =
+        "Вы внутри лифта";
+}
+
+function openDoors(){
+
+    document.getElementById("doorLeft").style.left =
+        "-100px";
+
+    document.getElementById("doorRight").style.right =
+        "-100px";
+}
+
+function closeDoors(){
+
+    document.getElementById("doorLeft").style.left =
+        "0px";
+
+    document.getElementById("doorRight").style.right =
+        "0px";
 }
